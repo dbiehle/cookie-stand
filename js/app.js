c090d57548aebb26fe01055d8d15e88f0a51657a
Drew Biehle <drew.biehle@gmail.com>
2017-06-12 17:14:59 -0700

    First & Pike location working and updating to sales.html

--- js/app.js
+++ js/app.js
@@ -20,24 +20,46 @@
 // separate object for each shop location
 var firstPike = {
   location: '1st and Pike',
+  storeHours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
   // stores min/max hourly customers into object property
   minCustomers: 23,
   maxCustomers: 65,
   // stores average cookies per customer into object property
   avgCookiesPerCustomer: 6.3,
   // uses a method to generate a random # of customers per hour
-  customersPerHour: 0,
   getCustomersPerHour: function(){
-    this.customersPerHour = randomCustomersPerHour(this.minCustomers, this.maxCustomers) + ' customers per hour';
-  },
-  // calculate and store simulated amount of cookies purchased for each hour using avg cookies purchased and the random number of customers generated
-  // resultsArray = [this.location, this.minCustomers, this.maxCustomers, this.avgCookiesPerCustomer, this.customersPerHour]
+    this.customersPerHour = randomCustomersPerHour(this.minCustomers, this.maxCustomers);
+  }
 };
+
+console.log(firstPike);
 
 function randomCustomersPerHour (min, max) {
   return Math.floor(Math.random() * (max - min + 1 ) + min);
+};
+
+var parentEl = document.getElementById('first');
+var article = document.createElement('article');
+parentEl.appendChild(article);
+var h2 = document.createElement('h2');
+h2.textContent = firstPike.location;
+article.appendChild(h2);
+var ul = document.createElement('ul');
+article.appendChild(ul);
+
+// firstPike.getCustomersPerHour();
+
+var totalCookies = 0;
+
+for (var i = 0; i < firstPike.storeHours.length; i++) {
+  var li = document.createElement('li');
+  firstPike.getCustomersPerHour();
+  var cookiesPerHour = Math.ceil(firstPike.customersPerHour * firstPike.avgCookiesPerCustomer);
+  li.textContent = firstPike.storeHours[i] + ': ' + cookiesPerHour + ' cookies';
+  ul.appendChild(li);
+  totalCookies += cookiesPerHour;
 }
 
-firstPike.getCustomersPerHour();
-
-console.log(firstPike);
+var liTotal = document.createElement('li');
+liTotal.textContent = 'Total: ' + totalCookies + ' cookies';
+ul.appendChild(liTotal);
