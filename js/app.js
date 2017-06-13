1e80930128fa18eb86053e863d70964d3fee9a98
Drew Biehle <drew.biehle@gmail.com>
2017-06-12 17:28:36 -0700

    Wrapped First & Pike into function storeCookiesGenerator and added other 4 shops

--- js/app.js
+++ js/app.js
@@ -17,49 +17,56 @@
 
 'use strict';
 
-// separate object for each shop location
-var firstPike = {
-  location: '1st and Pike',
-  storeHours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-  // stores min/max hourly customers into object property
-  minCustomers: 23,
-  maxCustomers: 65,
-  // stores average cookies per customer into object property
-  avgCookiesPerCustomer: 6.3,
-  // uses a method to generate a random # of customers per hour
-  getCustomersPerHour: function(){
-    this.customersPerHour = randomCustomersPerHour(this.minCustomers, this.maxCustomers);
+storeCookiesGenerator('1st and Pike', 'first', 23, 65, 6.3);
+storeCookiesGenerator('SeaTac Airport', 'seatac', 3, 24, 1.2);
+storeCookiesGenerator('Seattle Center', 'seattlecenter', 11, 38, 3.7);
+storeCookiesGenerator('Capitol Hill', 'caphill', 20, 38, 2.3);
+storeCookiesGenerator('Alki', 'alki', 2, 16, 4.6);
+
+function storeCookiesGenerator(loc, idName, minCus, maxCus, avgCookies) {
+  console.log(loc);
+  // separate object for each shop location
+  var shop = {
+    location: loc,
+    storeHours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+    // stores min/max hourly customers into object property
+    minCustomers: minCus,
+    maxCustomers: maxCus,
+    // stores average cookies per customer into object property
+    avgCookiesPerCustomer: avgCookies,
+    // uses a method to generate a random # of customers per hour
+    getCustomersPerHour: function(){
+      this.customersPerHour = randomCustomersPerHour(this.minCustomers, this.maxCustomers);
+    }
+  };
+
+  function randomCustomersPerHour (min, max) {
+    return Math.floor(Math.random() * (max - min + 1 ) + min);
+  };
+
+  var parentEl = document.getElementById(idName);
+  var article = document.createElement('article');
+  parentEl.appendChild(article);
+  var h2 = document.createElement('h2');
+  h2.textContent = shop.location;
+  article.appendChild(h2);
+  var ul = document.createElement('ul');
+  article.appendChild(ul);
+
+  // shop.getCustomersPerHour();
+
+  var totalCookies = 0;
+
+  for (var i = 0; i < shop.storeHours.length; i++) {
+    var li = document.createElement('li');
+    shop.getCustomersPerHour();
+    var cookiesPerHour = Math.ceil(shop.customersPerHour * shop.avgCookiesPerCustomer);
+    li.textContent = shop.storeHours[i] + ': ' + cookiesPerHour + ' cookies';
+    ul.appendChild(li);
+    totalCookies += cookiesPerHour;
   }
-};
 
-console.log(firstPike);
-
-function randomCustomersPerHour (min, max) {
-  return Math.floor(Math.random() * (max - min + 1 ) + min);
-};
-
-var parentEl = document.getElementById('first');
-var article = document.createElement('article');
-parentEl.appendChild(article);
-var h2 = document.createElement('h2');
-h2.textContent = firstPike.location;
-article.appendChild(h2);
-var ul = document.createElement('ul');
-article.appendChild(ul);
-
-// firstPike.getCustomersPerHour();
-
-var totalCookies = 0;
-
-for (var i = 0; i < firstPike.storeHours.length; i++) {
-  var li = document.createElement('li');
-  firstPike.getCustomersPerHour();
-  var cookiesPerHour = Math.ceil(firstPike.customersPerHour * firstPike.avgCookiesPerCustomer);
-  li.textContent = firstPike.storeHours[i] + ': ' + cookiesPerHour + ' cookies';
-  ul.appendChild(li);
-  totalCookies += cookiesPerHour;
+  var liTotal = document.createElement('li');
+  liTotal.textContent = 'Total: ' + totalCookies + ' cookies';
+  ul.appendChild(liTotal);
 }
-
-var liTotal = document.createElement('li');
-liTotal.textContent = 'Total: ' + totalCookies + ' cookies';
-ul.appendChild(liTotal);
