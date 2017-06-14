6b90145388b36400c34f29009016dbc294e721a5
Drew Biehle <drew.biehle@gmail.com>
2017-06-14 16:38:35 -0700

    Form added below table, Event handler and Event Listener functioning

--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ function CookieStores (shopName,minCustomers,maxCustomers,avgCookiesPerCustomer)
 };
 
 CookieStores.prototype.getCustomersPerHour = function(){
-  this.customersPerHour = randomNumberGenerator(this.minCustomers, this.maxCustomers);
+  return randomNumberGenerator(this.minCustomers, this.maxCustomers);
 };
 
 function randomNumberGenerator (min, max) {
-  return Math.floor(Math.random() * (max - min + 1 ) + min);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Initialize table and create header for table with store hours
@@ -40,8 +40,32 @@ function addHeader() {
   thead.appendChild(row);
 }
 
-// create body of table and method to populate the table with cookie needs and totals
+var foo = new CookieStores ('',0,0,0);
+foo.addHeader();
+
+// create body of table
 var tbody = document.createElement('tbody');
+
+// create form for adding new store locations to table
+var storeForm = document.getElementById('addStoreForm');
+storeForm.addEventListener('submit', addStoreHandler);
+
+function addStoreHandler(event){
+  event.preventDefault();
+  var shopName = event.target.shopName.value;
+  var minCustomers = event.target.minCustomers.value;
+  minCustomers = parseInt(minCustomers);
+  console.log('parseint minCustomers: ' + minCustomers);
+  var maxCustomers = event.target.maxCustomers.value;
+  maxCustomers = parseInt(maxCustomers);
+  console.log('parseint maxCustomers: ' + maxCustomers);
+  var avgCookiesPerCustomer = event.target.avgCookiesPerCustomer.value;
+  console.log('NO parseint avgCookiesPerCustomer: ' + avgCookiesPerCustomer);
+
+  var newCookieStore = new CookieStores(shopName,minCustomers,maxCustomers,avgCookiesPerCustomer);
+  newCookieStore.render();
+  storeForm.reset();
+}
 
 CookieStores.prototype.render = render;
 
@@ -55,8 +79,10 @@ function render() {
 
   for (var i = 0; i < this.storeHours.length; i++) {
     var td = document.createElement('td');
-    this.getCustomersPerHour();
+    this.customersPerHour = this.getCustomersPerHour();
+    console.log('customers per hour ' + this.customersPerHour);
     this.cookiesPerHour = Math.ceil(this.customersPerHour * this.avgCookiesPerCustomer);
+    // console.log(this.cookiesPerHour);
     td.textContent = this.cookiesPerHour;
     row.appendChild(td);
     this.totalCookies += this.cookiesPerHour;
@@ -68,19 +94,19 @@ function render() {
 }
 
 // create store location objects
-var firstPike = new CookieStores ('1st and Pike', 23, 65, 6.3);
-var seatac = new CookieStores ('SeaTac Airport', 3, 24, 1.2);
-var seattleCenter = new CookieStores ('Seattle Center', 11, 38, 3.7);
-var capHill = new CookieStores ('Capitol Hill', 20, 38, 2.3);
-var alki = new CookieStores ('Alki', 2, 16, 4.6);
+// var firstPike = new CookieStores ('1st and Pike', 23, 65, 6.3);
+// var seatac = new CookieStores ('SeaTac Airport', 3, 24, 1.2);
+// var seattleCenter = new CookieStores ('Seattle Center', 11, 38, 3.7);
+// var capHill = new CookieStores ('Capitol Hill', 20, 38, 2.3);
+// var alki = new CookieStores ('Alki', 2, 16, 4.6);
 
 // add cells and cookie needs and totals to table
-firstPike.addHeader();
-firstPike.render();
-seatac.render();
-seattleCenter.render();
-capHill.render();
-alki.render();
+// firstPike.addHeader();
+// firstPike.render();
+// seatac.render();
+// seattleCenter.render();
+// capHill.render();
+// alki.render();
 
 // Add footer
 // TODO: need to add totals per hour across locations to this footer
